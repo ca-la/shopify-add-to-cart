e97d37b03f932a492ac3956ae6dd5a36c84c332b
Dylan Pyle <me@dylanpyle.com>
2018-12-03 13:36:27 -0500

    Add test version

--- docs/add.js
+++ docs/add.js
@@ -1,1 +1,11 @@
-alert('Loaded add script');
+"use strict";
+exports.__esModule = true;
+function autoAddToCart(variantId) {
+    window.Shopify.onCartUpdate = function () {
+        var _a;
+        window.jQuery.post('/cart/update.js', {
+            updates: (_a = {}, _a[variantId] = 1, _a)
+        });
+    };
+}
+window.autoAddToCart = autoAddToCart;
