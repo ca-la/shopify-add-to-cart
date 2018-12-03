56f910b6f7e3ac3681b9cd8de2e269fb03207ca0
Dylan Pyle <me@dylanpyle.com>
2018-12-03 13:41:13 -0500

    Add UMD

--- docs/add.js
+++ docs/add.js
@@ -1,11 +1,21 @@
-"use strict";
-exports.__esModule = true;
-function autoAddToCart(variantId) {
-    window.Shopify.onCartUpdate = function () {
-        var _a;
-        window.jQuery.post('/cart/update.js', {
-            updates: (_a = {}, _a[variantId] = 1, _a)
-        });
-    };
-}
-window.autoAddToCart = autoAddToCart;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    function autoAddToCart(variantId) {
+        window.Shopify.onCartUpdate = function () {
+            var _a;
+            window.jQuery.post('/cart/update.js', {
+                updates: (_a = {}, _a[variantId] = 1, _a)
+            });
+        };
+    }
+    window.autoAddToCart = autoAddToCart;
+});
