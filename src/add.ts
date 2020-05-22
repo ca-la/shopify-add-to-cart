57dc6451bb4d3a725fb9c6a83d0afda29ad2079a
alevkon <alevkon@gmail.com>
2020-05-22 10:35:17 +0300

    Upgrade prettier

--- src/add.ts
+++ src/add.ts
@@ -1,36 +1,44 @@
 ///<reference path="add.d.ts"/>
 
-(function() {
+(function () {
   function post(url: string, data: object): Promise<any> {
     if (window.jQuery) {
       return window.jQuery.post(url, data);
     } else if (window.fetch) {
       return window.fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
     } else {
-      throw new Error('No compatible XHR wrapper found');
+      throw new Error("No compatible XHR wrapper found");
     }
   }
 
   function updateVariantCount(variantId: string) {
-    post('/cart/update.js', {
-      updates: { [variantId]: 1 }
+    post("/cart/update.js", {
+      updates: { [variantId]: 1 },
     });
   }
 
   function hideCartItems(variantId: string): void {
-    if (window.location.pathname !== '/cart') { return; }
-    const rows = Array.prototype.slice.call(document.querySelectorAll('.cart__row'));
+    if (window.location.pathname !== "/cart") {
+      return;
+    }
+    const rows = Array.prototype.slice.call(
+      document.querySelectorAll(".cart__row")
+    );
 
     for (const row of rows) {
-      const isVariantRow = Boolean(row.querySelector(`a[href*="${variantId}"]`))
-      if (!isVariantRow ) { continue; }
-      row.style.display = 'none';
+      const isVariantRow = Boolean(
+        row.querySelector(`a[href*="${variantId}"]`)
+      );
+      if (!isVariantRow) {
+        continue;
+      }
+      row.style.display = "none";
     }
   }
 
@@ -38,7 +46,7 @@
     updateVariantCount(variantId);
 
     if (!window.Shopify) {
-      console.error('No Shopify global found');
+      console.error("No Shopify global found");
       return;
     }
 
@@ -47,6 +55,6 @@
 
   window.ShopifyAddToCart = {
     autoAdd: registerListener,
-    hideCartItems
+    hideCartItems,
   };
 })();
